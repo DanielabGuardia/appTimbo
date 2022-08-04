7cf6d57e59605bd3f17a6785dd0c18c5aecc835b
DanielabGuardia <danielabguardia@gmail.com>
2022-08-04 16:12:56 -0300

    settings

--- src/app.js
+++ src/app.js
@@ -1,5 +1,36 @@
 const express = require('express');
 const app = express();
-/*const PORT = 3001;*/
+const path = require('path');
+const PORT = 3000;
 
-app.listen(3000, () => console.log('Server on port 3001'));
+const routes = require('./routes/index');
+const bodyParser = require('body-parser');
+
+/* Settings*/
+app.set('PORT', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views' ));
+app.set('view engine', 'ejs');
+
+
+/*Midlewares*/
+app.use((req, res, next) => {
+   console.log(`${req.url} -${req.method}`); 
+   next();
+})  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+/*Routes */
+app.use(routes);
+
+
+/* Static files*/
+/* --- views  errors*/
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}
+http://localhost:${PORT}`));
