260ea56a7c75584245a7b9d4ff6edf8845bf1b4d
DanielabGuardia <danielabguardia@gmail.com>
2022-09-09 15:36:36 -0300

    carouselHome

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,23 +1,10 @@
-const TIEMPO_INTERVALO_MILESIMAS_SEG = 4000;
-let slides = document.getElementsByClassName("slide");
-let n = 1
-for (i = 1; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}
-function pasarFoto(){
-    let i;
-    let slides = document.getElementsByClassName("slide");
-            
-    if (n > slides.length) { n = 1 };
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[n - 1].style.display = "block";
-    
-    n = n + 1
-    setTimeout(pasarFoto, 2000)
-}
+const myCarousel = document.getElementById('myCarousel')
 
-pasarFoto()
-// let intervalo = 0
-// intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
+myCarousel.addEventListener('slide.bs.carousel', event => {
+  // do something...
+})
+const myCarouselElement = document.querySelector('#myCarousel')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 5000,
+  wrap: false
+})
